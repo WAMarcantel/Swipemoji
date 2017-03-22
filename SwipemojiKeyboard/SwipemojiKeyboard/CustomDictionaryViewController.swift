096279f2d1b9a2447d7ae8196a7401ea4d007271
iTwicKz <twickes32@ufl.edu>
2017-03-22 10:55:46 -0400

    Prepare for merge with prediction bar

---
+++ SwipemojiKeyboard/SwipemojiKeyboard/CustomDictionaryViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CustomDictionaryViewController.swift
+//  SwipemojiKeyboard
+//
+//  Created by Takashi Wickes on 3/21/17.
+//  Copyright © 2017 swipemoji. All rights reserved.
+//
+
+import UIKit
+
+class CustomDictionaryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var _library = PointCloudLibrary.getDemoLibrary()
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dictionaryEntryCell", for: indexPath) as! DictionaryEntryTableViewCell
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            self.dawgs.removeAtIndex(indexPath.row)
+            self.tableView.reloadData()
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwipemojiKeyboard/SwipemojiKeyboard/DictionaryEntryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DictionaryEntryTableViewCell.swift
+//  SwipemojiKeyboard
+//
+//  Created by Takashi Wickes on 3/21/17.
+//  Copyright © 2017 swipemoji. All rights reserved.
+//
+
+import UIKit
+
+class DictionaryEntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
